7347daf67a27d97389b416bbd184d4f55a465396
Antony rossi de jesus <antonyrossidejesus@gmail.com>
2024-08-14 20:13:34 -0300

    att2 assets and html and js

--- src/js/popup-menu.js
+++ src/js/popup-menu.js
@@ -1,8 +1,17 @@
 const hamburger = document.getElementById("hamburger");
 const popupMenu = document.getElementById("popupMenu");
+const menuItems = popupMenu.querySelectorAll("a");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   popupMenu.classList.toggle("active");
   document.body.classList.toggle("no-scroll");
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    popupMenu.classList.remove("active");
+    document.body.classList.remove("no-scroll");
+  });
 });
